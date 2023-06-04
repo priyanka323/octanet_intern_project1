e9fe011a85549ee6638eb527825de6f94b0afffd
Ravi <raviswami899@gmail.com>
2023-06-04 18:27:08 +0530

    static pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Page_1 from './pages/page1'
+import Page_2 from './pages/page2';
+import Page_3 from './pages/page3';
+import Page_4 from './pages/page4';
+import Page_5 from './pages/page5';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Page_1/>
+    <Page_2/>
+    <Page_3/>
+    <Page_4/>
+    <Page_5/>
+    </>
   );
 }
 
---
+++ src/pages/page5.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Page_5 ()  {
+    // const image1 = require('./octanet.png')
+    return(
+        <>
+        <hr></hr>
+        <div style={{height:'543px'}}>
+            <div >
+                <img src='https://octanet.in/storage/2023/04/cropped-cropped-sdrf-300x134.png' alt='img logo' style={{marginLeft:'900px'}} />
+            </div>
+            <div>
+                <h1>WEB DEVELOPMENT</h1>
+            </div>
+            <div>
+                <h1>Task</h1>
+                <h1>List</h1>
+            </div>
+            
+        </div>
+        </>
+    )
+};
+export default Page_5;
