cccb6571acee7d499068897f4f3d7cd66b4b9cc1
Ravi <raviswami899@gmail.com>
2023-06-04 18:54:32 +0530

    task1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Page_1 from './pages/page1'
+import Page_2 from './pages/page2';
+import Page_3 from './pages/page3';
+import Page_4 from './pages/page4';
+import Page_5 from './pages/page5';
+
+function App() {
+  return (
+    <>
+    <Page_1/>
+    <Page_2/>
+    <Page_3/>
+    <Page_4/>
+    <Page_5/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/page3.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Page_3 ()  {
+    // const image1 = require('./octanet.png')
+    return(
+        <>
+        <hr></hr>
+        <div style={{height:'543px'}}>
+            <div>
+                <img src='https://octanet.in/storage/2023/04/cropped-cropped-sdrf-300x134.png' alt='img logo' />
+            </div>
+            <div style={{display:'flex', flexdirection:'row'}}>
+                <div style={{width:'50%',marginLeft:'6%',marginRight:'6%'}}>
+                    <h1>LANDING PAGE</h1>
+                    <p>A landing page is one of the best web development
+                    projects for beginners. A foundational understanding of
+                    HTML and CSS is required. You will learn how to add
+                    columns, divide sections, arrange items, add headers,
+                    footers. Create an impressive page using your creativity.
+                    It is important to pay attention to the alignment, the
+                    padding, the color palette, and boxes. Make sure CSS
+                    doesn't overlap elements.
+                    </p>
+                </div>
+                <div style={{width:'50%'}}>
+                    <img src='https://wpklik.com/wp-content/uploads/2021/05/Portfolio-Website-Templates-HTML-templates-for-Creatives.jpg'style={{height:'300px',width:'500x',marginLeft:'6%'}}/>
+                </div>
+                
+            </div>
+            
+        </div>
+        </>
+    )
+};
+export default Page_3;
